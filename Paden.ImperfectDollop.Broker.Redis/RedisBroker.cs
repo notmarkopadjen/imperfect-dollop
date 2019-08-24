c74b6d412f073f746bbb1778746e9da46e734817
notmarkopadjen <p@djen.email>
2019-08-24 02:48:25 +0200

    RedisBroker initial

---
+++ Paden.ImperfectDollop.Broker.Redis/RedisBroker.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Paden.ImperfectDollop.Broker.Redis
+{
+    public class RedisBroker : IBroker
+    {
+        static readonly Lazy<IConfigurationRoot> config;
+        static readonly Lazy<string> redisUri;
+
+        static RedisBroker()
+        {
+            config = new Lazy<IConfigurationRoot>(() => new ConfigurationBuilder().AddJsonFile("appsettings.json", false, false).AddEnvironmentVariables().Build());
+            redisUri = new Lazy<string>(() => config.Value["Redis:Uri"]);
+        }
+
+        public bool IsMultiThreaded => false;
+
+        public void Dispose()
+        {
+
+        }
+
+        public void StartFor<T, TId>(Repository<T, TId> repository) where T : Entity<TId>, new()
+        {
+
+        }
+    }
+}
---
+++ Paden.ImperfectDollop.Integration.Tests/StudentRepositoryWithRedisBrokerTests.cs
@@ -0,0 +1,17 @@
+﻿using Paden.ImperfectDollop.Broker.Redis;
+using Xunit.Abstractions;
+
+namespace Paden.ImperfectDollop.Integration.Tests
+{
+    public class StudentRepositoryWithRedisBrokerTests : StudentRepositoryTests
+    {
+        public StudentRepositoryWithRedisBrokerTests(DatabaseFixture fixture, ITestOutputHelper output) : base(fixture, output)
+        {
+        }
+
+        protected override IBroker CreateBroker()
+        {
+            return new RedisBroker();
+        }
+    }
+}
